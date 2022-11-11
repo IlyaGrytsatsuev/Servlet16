d03f41ae664224f64e1db212f480a62ba88b84c5
IlyaGrytsatsuev <71152247+IlyaGrytsatsuev@users.noreply.github.com>
2022-11-11 19:50:00 +0300

    Add files via upload
    Working properly

--- Servlet16/ListOpener.js
+++ Servlet16/ListOpener.js
@@ -1,3 +1,6 @@
+
+var state ;
+
 function openList(count) {
     var Button = document.getElementById(count + "Button");
     var DownList = document.getElementById(count);
@@ -15,6 +18,30 @@ function openList(count) {
     }
     
  } 
+ 
+ 
+ function deleteAll(size){
+ 
+ var xhr = new XMLHttpRequest();
+ 
+ var data = "state=delete";
+   
+
+    xhr.onreadystatechange = function(){
+        if(this.readyState == 4 && this.status == 200){
+		hideSubLists(size);
+        }
+    }
+
+    xhr.open("POST", "/Servlet16/List", true);
+
+    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+
+    xhr.send(data);
+
+	
+ 
+ }
 
 
 function hide(count){
@@ -26,7 +53,45 @@ function show(element){
 	element.style.display = 'block';
 }
 
+function changeVisibility(size){
+	
+	if(state)
+		hideAll(size);
+		
+	else if(!state)
+		showAll(size);
+
+}
+
+function showAll(size){
+    
+    for(var i = 1; i < size; i++){
+	        document.getElementById(i + "Button").value = "-";
+		show(document.getElementById(i));
+    }
+    state = true;
+}
+
 function hideAll(size){
-	for(var i = 1; i <=size; i++)
+	for(var i = 1; i < size; i++){
+	        document.getElementById(i + "Button").value = "+";
+		hide(i);
+    }
+    	state = false;
+}
+
+function hideSubLists(size){
+	for(var i = 1; i < size; i++){
+		var Button = document.getElementById(i + "Button");
+		Button.style.display = 'none';
+    		hide(i);
+    	}
+}
+
+
+function Start(size){
+	state = false;
+	for(var i = 1; i < size; i++)
     			hide(i);
 }
+
